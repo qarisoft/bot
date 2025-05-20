e5be127db1229a96164439dbdee5757336ed5185
Salah Al-Salahi <cloud.data.sa@gmail.com>
2025-05-21 00:13:22 +0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/bot/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
